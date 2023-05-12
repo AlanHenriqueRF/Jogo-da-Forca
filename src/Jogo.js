153a78b51236e5bcdb808e40f9620bd618a9a670
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-05-12 14:16:07 -0300

    Versão com pré-jogo pronto

--- src/Jogo.js
+++ src/Jogo.js
@@ -1,7 +1,15 @@
+import forca from "./assets/forca0.png"
+// import cabeca from "./assets/forca1.png"
+// import corpo from "./assets/forca2.png"
+// import braco1 from "./assets/forca3.png"
+// import braco2 from "./assets/forca4.png"
+// import perna1 from "./assets/forca5.png"
+// import perna2 from "./assets/forca6.png"
+
 export default function Jogo() {
     return (
         <div className="menu">
-            <div className="forca"><img src="./assets/forca0.png" alt="Imagem forca" /></div>
+            <div className="forca"><img src={forca} alt="Imagem forca" /></div>
             <div className="acao">
                 <button>Escolher palavra</button>
                 <div className="palavra">_ua _e _ _e _a</div>
--- src/Letras.js
+++ src/Letras.js
@@ -1,33 +1,10 @@
 export default function Letras() {
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let letras = alfabeto.map(letra=><button key={letra} disabled>{letra.toUpperCase()}</button>)//<li className="letra"></li> <ul></ul>
     return (
         <div className="rodape">
             <div className="alfabeto">
-                <button disabled>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
-                <button>A</button>
+                {letras}
             </div>
         </div>);
 }
